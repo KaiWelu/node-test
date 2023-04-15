5cd635b8c9793f91023cea2158a6f2bcd1a0c85a
Kai Weluda <kaiweluda@gmail.com>
2023-04-15 17:40:51 +0200

    feat: adds a test POST request to the server

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const app = express();
 app.listen(3000, () => console.log("--Hi, I'm listening at port 3000--"));
 //this serves as static file from the folder "public"
 app.use(express.static("public"));
+//this is for receiving data
+app.post("/api", (request, response) => {
+  console.log(request.body);
+});
