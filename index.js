51e90065ee8f43d4b9f216f821dfba003a5c325b
Kai Weluda <kaiweluda@gmail.com>
2023-04-15 18:06:12 +0200

    feat: adds a complete test handshake between client and server

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const app = express();
 app.listen(3000, () => console.log("--Hi, I'm listening at port 3000--"));
 //this serves as static file from the folder "public"
 app.use(express.static("public"));
+//this enables the server to read the request as JSON
+app.use(express.json({ limit: "1mb" }));
 //this is for receiving data
 app.post("/api", (request, response) => {
+  console.log("I got a request incoming!");
   console.log(request.body);
+  //sends back a JSON object with the received data (lol)
+  response.json({
+    status: "success",
+    latitude: request.body.latitude,
+    longitude: request.body.longitude,
+  });
 });
